a2274eb743f53af4bce5159db2b93c9bbe0f5790
Jon Rice <revyolution1120@gmail.com>
2019-06-21 14:32:53 -0700

    Showing hoods without empty strings

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -27,16 +28,39 @@ namespace LINQ
                 //Console.WriteLine($"{o}");
                 //Console.ReadLine();
 
-                    var postDaHood =
-                from p in o["features"]
-                select (string)p["properties"]["neighborhood"];
+                var postDaHood =
+                             from p in o["features"]
+                             select (string)p["properties"]["neighborhood"];
+                IEnumerable<string> notEmpty = postDaHood.Where(hood => hood != "");
 
-                foreach (var item in postDaHood)
+                foreach (var item in notEmpty)
                 {
                     Console.WriteLine(item);
                 }
             }
         }
+
+        //static void NoEmpty()
+        //{
+        //    using (StreamReader reader = File.OpenText(@"C:\Users\RevYolution\source\repos\LINQ\LINQ\data.json"))
+
+        //    {
+        //        JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+        //        //Console.WriteLine($"{o}");
+        //        //Console.ReadLine();
+
+        //        var postDaHood =
+        //                        from p in o["features"]
+        //                        where ["features.properties.neighborhood > 0"];
+        //                        select (string)p["properties"]["neighborhood"];
+
+        //        foreach (var item in postDaHood)
+        //        {
+        //            Console.WriteLine(item);
+        //        }
+        //    }
+
+        //}
 
     }
 }
