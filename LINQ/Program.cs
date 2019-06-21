965b10f594906bdb3938f904cdab26800785825f
Jon Rice <revyolution1120@gmail.com>
2019-06-21 09:35:33 -0700

    Add project files.

---
+++ LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        StreamReader reader = File.OpenText(@"c:\person.json");
+        
+    JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+    // do stuff
+        
+    }
+}
