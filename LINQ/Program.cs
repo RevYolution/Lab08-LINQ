84295726d0caeb0c77cca8eb3078d1f4a116007b
Jon Rice <revyolution1120@gmail.com>
2019-06-21 13:27:15 -0700

    Able to grab all hoods.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,21 +1,42 @@
-﻿using Newtonsoft.Json;
+﻿//using LINQ.Classes;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace LINQ
 {
     class Program
     {
+        public static readonly string filePath = "../data.json";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            OutPut();
+            //Test();
         }
 
-        StreamReader reader = File.OpenText(@"c:\person.json");
-        
-    JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
-    // do stuff
-        
+        static void OutPut()
+        {
+            using (StreamReader reader = File.OpenText(@"C:\Users\RevYolution\source\repos\LINQ\LINQ\data.json"))
+
+            {
+                JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+                //Console.WriteLine($"{o}");
+                //Console.ReadLine();
+
+                    var postDaHood =
+                from p in o["features"]
+                select (string)p["properties"]["neighborhood"];
+
+                foreach (var item in postDaHood)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
     }
 }
